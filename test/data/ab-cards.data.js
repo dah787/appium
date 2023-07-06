d24e4e812f44bcd5ba88a6b393ca4e9f95f03c76
dah787 <59451267+dah787@users.noreply.github.com>
2023-07-06 15:33:15 +0500

    Add files via upload

--- test/data/ab-cards.data.js
+++ test/data/ab-cards.data.js
@@ -1,1 +1,70 @@
+class CardsData {
 
+/* CONSTANTS */
+// phoneNum_Registered_pass = 'qwerty123'; // related to '99 966 46 60','94 315 46 60'
+// cardName_Registered = 'Additional Card'; //'Дополнительная';
+// phoneNum_Registered_A_noCards = '94 315 46 60';
+// cardNum_Registered = '5614 6871 0785 3401';
+// cardExpiryDate_Registered = '02 26';
+
+// Cards-1 on 31.05.2023: phone numbers registered and have added cards
+phoneNum_5_hasCards = '99 966 46 60'; // +998999664660 Kuvandikov Anvarbek
+phoneNum_5_pass = 'qwerty123';
+  cardName_Humo_5 = 'Humo-5'; // неизвестно
+  cardNum_Humo_5 = '9860 0901 0197 9368';
+  cardExp_Humo_5 = '10 26';
+cardName_Uzcard_5 = 'Uzcard-5'; // неизвестно
+cardNum_Uzcard_5 = '5614 6871 0135 7581';
+cardExp_Uzcard_5 = '03 26';
+
+phoneNum_10_hasCards = '99 392 30 75'; // +998993923075 Alisher Djurayev
+phoneNum_10_pass = 'qwerty123';
+cardName_Humo_10 = 'Humo-10'; // неизвестно
+cardNum_Humo_10 = '9860 0901 0156 4376';
+cardExp_Humo_10 = '10 26';
+
+// Cards-2 on 31.05.2023: phone numbers registered and have added cards, but not connected to phone number in Humo system
+phoneNum_1_hasCards = '91 781 39 87'; // +998917813987 Artyom Ivashchenko
+phoneNum_1_pass = 'qwerty123';
+phoneNum_1_holderName = 'Artyom';
+phoneNum_1_holderSurname = 'Ivashchenko';
+  cardName_Humo_1 = 'Humo-1'; // неизвестно
+  cardNum_Humo_1 = '9860 0901 0150 5825';
+  cardExp_Humo_1 = '10 26';
+cardName_Uzcard_1 = 'Uzcard-1'; // неизвестно
+cardNum_Uzcard_1 = '5614 6871 0856 4551';
+cardExp_Uzcard_1 = '03 26';
+
+// Cards-2 on 17.05.2023: phone numbers registered, but no added cards
+
+// Cards-3 on 15.05.2023: phone numbers not registered, but before did and had added cards
+phoneNum_2_hasCards = '97 412 88 88'; // +998974128888 Asadov Azizbek
+phoneNum_2_pass = 'qwerty123';
+  cardName_Humo_2 = 'Humo-2'; // неизвестно
+  cardNum_Humo_2 = '9860 0901 0100 3136';
+  cardExp_Humo_2 = '10 26';
+cardName_Uzcard_2 = 'Uzcard-2'; // неизвестно
+cardNum_Uzcard_2 = '5614 6871 042 76655';
+cardExp_Uzcard_2 = '03 26';
+
+phoneNum_3_hasCards = '98 313 11 11'; // +998983131111 Kamoliddin aka
+phoneNum_3_pass = 'qwerty123';
+cardName_Uzcard_3 = 'Uzcard-3'; // неизвестно
+cardNum_Uzcard_3 = '5614 6871 0840 5300';
+cardExp_Uzcard_3 = '03 26';
+
+phoneNum_4_hasCards = '99 887 72 18'; // 998998877218 Amanov Sardor
+phoneNum_4_pass = 'qwerty123';
+  cardName_Humo_4 = 'Humo-4'; // неизвестно
+  cardNum_Humo_4 = '9860 0901 0173 1322';
+  cardExp_Humo_4 = '10 26';
+cardName_Uzcard_4 = 'Uzcard-4'; // неизвестно
+cardNum_Uzcard_4 = '5614 6871 0443 3470';
+cardExp_Uzcard_4 = '03 26';
+
+
+
+/* EOF class */
+}
+
+module.exports = new CardsData();
