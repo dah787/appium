5c47e4386f328cbb08e85e19e461484b0df06a79
dah787 <59451267+dah787@users.noreply.github.com>
2023-07-13 15:03:13 +0500

    Add files via upload

--- test/utils/android/ab-app.utils.js
+++ test/utils/android/ab-app.utils.js
@@ -93,6 +93,47 @@ async generateElementListInArray(raw_array, data_array, elementAttributeKey, ele
   // /*отладка*/ console.log('\n --> data_array[1] = ' + await data_array[1].getAttribute('resource-id') + '\n');
 }
 
+async generateElementListInArray_Debug(raw_array, data_array, elementAttributeKey, elementAttributeValue_1, elementAttributeValue_2) { // appList(raw_array, ...)
+  let elementAttributeValue_Current = '';
+  
+  for(let i = 0, l = raw_array.length; i < l; i++) { // for (const element of raw_array) {
+    // elementAttributeValue_Current = await element.getAttribute('resource-id');
+    elementAttributeValue_Current = await raw_array[i].getAttribute(elementAttributeKey);
+    if(elementAttributeValue_Current == elementAttributeValue_1){
+      // /*отладка*/ console.log(
+      //   '\n --> ' + elementAttributeValue_1       + ' = elementAttributeValue_1' +
+      //   '\n --> ' + elementAttributeValue_Current + ' = elementAttributeValue_Current' + '\n');
+      // /*отладка*/ await driver.pause(5000);
+      data_array.push(raw_array[i]); // .push(element)
+
+      for(let y = i+1; y < l; y++) {
+        elementAttributeValue_Current = await raw_array[y].getAttribute(elementAttributeKey);
+        if(elementAttributeValue_Current == elementAttributeValue_2){
+          // /*отладка*/ console.log(
+          //   '\n --> ' + elementAttributeValue_2       + ' = elementAttributeValue_2' +
+          //   '\n --> ' + elementAttributeValue_Current + ' = elementAttributeValue_Current' + '\n');
+          // /*отладка*/ await driver.pause(5000);
+          data_array.push(raw_array[y]);
+          y = l; // terminates second loop // break;
+        }
+      }
+    }
+  }
+  // /*отладка*/ console.log(
+  //   '\n --> ' + await data_array[0].getAttribute('resource-id') + ' = data_array[0]' +
+  //   '\n --> ' + await data_array[1].getAttribute('resource-id') + ' = data_array[1]' +
+  //   '\n --> ' + await data_array[2].getAttribute('resource-id') + ' = data_array[2]' +
+  //   '\n --> ' + await data_array[3].getAttribute('resource-id') + ' = data_array[3]' +
+  //   '\n');
+  // /*отладка*/ console.log(
+  //   '\n --> ' + await data_array[0].getText() + ' = data_array[0].getText()' +
+  //   '\n --> ' + await data_array[1].getText() + ' = data_array[1].getText()' +
+  //   '\n --> ' + await data_array[2].getText() + ' = data_array[2].getText()' +
+  //   '\n --> ' + await data_array[3].getText() + ' = data_array[3].getText()' +
+  //   '\n');
+  // /*отладка*/ await driver.pause(15000);
+}
+
 async generateRandomChars(length, charType) { // randomChars(length, charType)
   // Declare all characters
   // const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -115,6 +156,39 @@ async generateRandomChars(length, charType) { // randomChars(length, charType)
     default:
       chars = '0123456789';
       break;
+  }
+
+  // Pick characers randomly
+  let str = '';
+  for (let i = 0; i < length; i++) {
+      str += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  return str;
+}
+
+async generateRandomCharsOfSet(length, charSet, charType) {
+  let chars = '';
+  if(charSet){
+    chars = charSet;
+  } else {
+    switch (charType) {
+      case 'en':
+        chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        break;
+
+      case 'ru': // ! неизвестны коды кириллицы, используемые в appKeyboardTypeIn(value)
+        chars = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя0123456789';
+        break;
+
+      case 'amount':
+        chars = '123456789';
+        break;
+
+      default:
+        chars = '0123456789';
+        break;
+    }
   }
 
   // Pick characers randomly
